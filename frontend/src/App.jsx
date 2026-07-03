e8b9868edb4f9606576047404b9c2aebf536399d
adhe2 <adheranggamhmd@gmail.com>
2026-07-03 16:55:41 +0900

    Pembuatan halaman login

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Login from "./components/Login.jsx";
+import "./css/App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
